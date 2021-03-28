c18ae399f8e86ed5f47c14eaeb8a8b45dec753b9
ishwarg <67804752+ishwarg@users.noreply.github.com>
2021-03-28 14:05:42 -0700

    Ishwarjot

---
+++ Drag.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rocket_Simulator
+{
+    class Drag
+    {
+        public double wetA;
+        public static double Radius = 0.169 / 2;
+        public static double Radius2 = 0.173 / 2;
+        public static double Aref = Radius * Radius2 * Math.PI;
+        public double stage;
+        public double drag;
+        public double Cd;
+        
+
+        public Drag(double area)
+        {
+            wetA = area;
+        }
+
+        public void wetarea(Fin fin1,Fin fin2, int stage)
+        {
+
+            //for greater wetted Area accuracy include the area of the leading edge and trailing edge of fins
+
+            if (stage == 1)
+            {
+                wetA = 8 * fin1.Area + 8 * fin2.Area + 179988.81 * 0.001 * 0.001 + 2 * Math.PI * Rocket.Radius * (1.895 - 0.5 + 4.02 - 2.22) + 2 * Math.PI * Rocket.Radius2 * 0.3 + 4 * fin1.TC * 0.003 + 4 * fin2.TC * 0.003;
+
+            }
+            else
+                wetA = 179988.81 * 0.001 * 0.001 + 2 * Math.PI * Rocket.Radius * (1.895 - 0.5) + 2 * Math.PI * Rocket.Radius2 * 0.3;
+
+        }
+
+        public double skin(Conditions conditions,Rocket rocket, int stage,Fin fin1, Fin fin2)
+        {
+            double sdrag=0;
+            double viscosity=0.558219*Math.Pow(1.00017,0.999946*(rocket.altitude+conditions.launch_elevation))+0.769948;
+            double Re = rocket.V * Rocket.Radius * 2/viscosity;
+            double Cf;
+            double Rcrit;
+            double Cfc;
+            double Cd;
+            double wetfin;
+           
+
+
+            if (stage == 1)
+            {
+                wetfin = 8 * fin1.Area + 8 * fin2.Area+ 4 * fin1.TC * 0.003 + 4 * fin2.TC * 0.003;
+                Rcrit = 51 * Math.Pow(60*0.000001/4.02,-1.039);
+                if (Re < 10000)
+                    Cf = 0.0148;
+                else if (Re >= 10000 && Re < Rcrit)
+                    Cf = 1 / (1.5 * Math.Log(Re) - 5.6) / (1.5 * Math.Log(Re) - 5.6);
+                else
+                    Cf = 0.032 * Math.Pow(60 * 0.000001 / 4.02, 0.2);
+
+            }
+            else
+            {
+                wetfin =8 * fin2.Area  + 4 * fin2.TC * 0.003;
+                Rcrit = 51 * Math.Pow(60 * 0.000001 / 2.22, -1.039);
+                if (Re < 10000)
+                    Cf = 0.0148;
+                else if (Re >= 10000 && Re < Rcrit)
+                    Cf = 1 / (1.5 * Math.Log(Re) - 5.6) / (1.5 * Math.Log(Re) - 5.6);
+                else
+                    Cf = 0.032 * Math.Pow(60 * 0.000001 / 2.22, 0.2);
+            }
+
+            if (rocket.Mach(conditions) <= 0.8)
+            {
+                Cfc = Cf*(1 - 0.1 * rocket.Mach(conditions) * rocket.Mach(conditions));
+            }
+            else
+            {
+                Cfc = Cf / Math.Pow((1 + rocket.Mach(conditions) * rocket.Mach(conditions) * 0.15),0.58);
+            }
+
+            if (Cf / (1 + 0.18 * rocket.Mach(conditions) * rocket.Mach(conditions)) > Cfc)
+                Cfc = Cf / (1 + 0.18 * rocket.Mach(conditions) * rocket.Mach(conditions));
+
+            
+
+            Cd = Cfc * (1+1/2/(rocket.length/2/Rocket.Radius2)*wetA);
+
+            
+            sdrag = 0.5 * Cfc * conditions.rho * rocket.V * rocket.V * wetA;
+
+            return sdrag;
+        }
+
+        public double pressure(Conditions conditions, Rocket rocket, int stage)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Simulator.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+
+
+namespace Rocket_Simulator
+{
+    class Simulator
+    {
+        public static double[,] LowerThrust = new double[15, 2];
+        public static double[,] UpperThrust = new double[22, 2];
+        public static double delay = 4;
+        public static string path1 = "C:/Users/ishwa/Downloads/Cesaroni_21062O3400-P.csv";
+        public static string path2 = "C:/Users/ishwa/Downloads/Cesaroni_10133M795-P.csv";
+        public static string path3 = "C:/Users/ishwa/Downloads/Output.csv";
+
+
+
+
+        public static double tstep = 0.01;
+
+
+
+
+        static void Main(string[] args)
+        {
+            double time = 0.0;
+           
+
+            Rocket first_stage = new Rocket(path1, 6.1, 21041.0, 11.272, 54.326,4.02);
+            Rocket second_stage = new Rocket(path2, 12.76, 10133, 4.892, 26.072,2.22);
+            Fin lowerstage = new Fin(0.166, 0.3, 0.06, 0.22, 3.72);
+            Fin upperstage = new Fin(0.16, 0.3, 0.24, 0.04, 1.9);
+            Conditions sim1 = new Conditions();
+            System.IO.File.WriteAllText(path3, string.Empty);
+
+            using (StreamWriter writer = new StreamWriter(path3, true))
+            {
+                writer.WriteLine("time,thrust,drag,coeffcient,A,Az,V,Vz,Altitude");
+            }
+
+            Functions.csvReader(path1, LowerThrust);
+            Functions.csvReader(path2, UpperThrust);
+
+            sim1.setConditions(first_stage, 5, 0);
+
+
+            while (time <= first_stage.burn_time + delay)
+            {
+                    first_stage.avx(time, sim1, 1,time);
+                    first_stage.Az = first_stage.A * Math.Cos(first_stage.phi * Math.PI / 180);
+                    first_stage.altitude += first_stage.partialD * Math.Cos(first_stage.phi * Math.PI / 180);
+                    first_stage.mass = first_stage.totalmass(1);
+                    sim1.setConditions(first_stage, 5, 0);
+                first_stage.Vz = first_stage.V * Math.Cos(first_stage.phi * Math.PI / 180);
+
+                using (StreamWriter writer = new StreamWriter(path3, true))
+                {
+                    writer.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8}", time, first_stage.thrust, first_stage.drag(sim1, 1), first_stage.Cd1(sim1), first_stage.A, first_stage.Az, first_stage.V, first_stage.Vz, first_stage.altitude);
+                }
+
+                //Console.WriteLine("{0}, {1}", time, first_stage.V);
+
+                time += 0.01;
+
+            }
+            second_stage.altitude = first_stage.altitude;
+            sim1.setConditions(second_stage, 5, 0);
+            second_stage.V = first_stage.V;
+            second_stage.Vz = second_stage.V*Math.Cos(second_stage.phi * Math.PI / 180);
+         
+
+           
+            while (second_stage.Vz>0)
+            {
+                
+                second_stage.avx(time, sim1, 2,first_stage.burn_time+delay);
+                second_stage.Az = second_stage.A * Math.Cos(second_stage.phi * Math.PI / 180);
+                second_stage.Vz = second_stage.V * Math.Cos(second_stage.phi * Math.PI / 180);
+                second_stage.altitude += second_stage.partialD * Math.Cos(second_stage.phi * Math.PI / 180);
+                second_stage.mass = second_stage.totalmass(2);
+                sim1.setConditions(second_stage, 5, 0);
+                time += 0.01;
+
+                using (StreamWriter writer = new StreamWriter(path3, true))
+                {
+                    writer.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8}", time, second_stage.thrust, second_stage.drag(sim1, 1), second_stage.Cd2(sim1), second_stage.A, second_stage.Az, second_stage.V, second_stage.Vz, second_stage.altitude);
+                }
+
+
+
+
+            }
+
+            
+
+            Console.WriteLine("{0:0.00}",second_stage.altitude*3.281);
+            Console.WriteLine(time);
+
+
+
+        }
+    }
+    
+
+
+}
+
+
